28182c97ab675ccced88435d873553860fde0c4e
Michael Abel <mabel@bitmuster.org>
2021-03-06 23:42:53 +0100

    Add minimal four phase

---
+++ 002_Minimal_four_phase/run.py
@@ -0,0 +1,19 @@
+import pysys
+from pysys.constants import *
+from pysys.basetest import BaseTest
+
+
+class PySysTest(BaseTest):
+    def setup(self):
+        self.log.info("Setup")
+        self.addCleanupFunction(self.cleanup)
+
+    def execute(self):
+        self.log.info("Execute")
+
+    def validate(self):
+        self.log.info("Validate")
+        self.addOutcome(PASSED)
+
+    def cleanup(self):
+        self.log.info("Cleanup")
